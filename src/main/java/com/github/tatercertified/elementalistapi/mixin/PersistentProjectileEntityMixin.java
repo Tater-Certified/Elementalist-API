8fce65f9b7480ab72511c91f8c1e7b8bfe21316a
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2023-01-17 21:05:10 -0500

    Cursed projectile spells
    Yes, very cursed

---
+++ src/main/java/com/github/tatercertified/elementalistapi/mixin/PersistentProjectileEntityMixin.java
@@ -0,0 +1,50 @@
+package com.github.tatercertified.elementalistapi.mixin;
+
+import com.github.tatercertified.elementalistapi.spell.target.TargetEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PersistentProjectileEntity.class)
+public abstract class PersistentProjectileEntityMixin extends ProjectileEntity {
+
+    public PersistentProjectileEntityMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    /**
+     * Makes TargetEntities not have gravity applied
+     * @return returns original variable if not a TargetEntity
+     */
+    @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;multiply(D)Lnet/minecraft/util/math/Vec3d;"))
+    private double inject(double d) {
+        if (((Object) this) instanceof TargetEntity) {
+            return 1.00f;
+        } else {
+            return d;
+        }
+    }
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/projectile/PersistentProjectileEntity;setPosition(DDD)V"))
+    public void tick(CallbackInfo ci) {
+        setVelocityClient(this.getVelocity().x, this.getVelocity().y, this.getVelocity().z);
+    }
+
+
+    /**
+     * Makes sure to prevent the arrow damaging entities
+     */
+    @Inject(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;ceil(D)I"), cancellable = true)
+    private void onEntityHit(EntityHitResult entityHitResult, CallbackInfo ci) {
+        if (((Object) this) instanceof TargetEntity) {
+            ci.cancel();
+        }
+    }
+}
--- src/main/java/com/github/tatercertified/elementalistapi/spell/target/TargetEntity.java
+++ src/main/java/com/github/tatercertified/elementalistapi/spell/target/TargetEntity.java
@@ -1,20 +1,33 @@
 package com.github.tatercertified.elementalistapi.spell.target;
 
+import com.github.tatercertified.elementalistapi.spell.BasicSpell;
 import eu.pb4.polymer.core.api.entity.PolymerEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class TargetEntity extends PersistentProjectileEntity implements PolymerEntity {
 
     protected float speed;
-
-    public TargetEntity(EntityType<ArrowEntity> entityType, World world, float speed) {
+    protected BasicSpell spell;
+    protected double distance = 100;
+    public Vec3d start_pos;
+    public TargetEntity(EntityType<ArrowEntity> entityType, World world, float speed, BasicSpell spell) {
         super(entityType, world);
         this.speed = speed;
+        this.spell = spell;
+    }
+    public TargetEntity(EntityType<ArrowEntity> entityType, World world, float speed, BasicSpell spell, double distance) {
+        super(entityType, world);
+        this.speed = speed;
+        this.spell = spell;
+        this.distance = distance;
     }
 
     @Override
@@ -35,5 +48,43 @@ public class TargetEntity extends PersistentProjectileEntity implements PolymerE
     @Override
     public EntityType<?> getPolymerEntityType(ServerPlayerEntity player) {
         return EntityType.ARROW;
+    }
+
+    @Override
+    public void tick() {
+        if (checkDistance()) {
+            hitBlock();
+        }
+        super.tick();
+    }
+
+    @Override
+    protected void onBlockHit(BlockHitResult blockHitResult) {
+        hitBlock();
+        super.onBlockHit(blockHitResult);
+    }
+
+    @Override
+    protected void onEntityHit(EntityHitResult entityHitResult) {
+        hitEntity();
+        super.onEntityHit(entityHitResult);
+    }
+
+    /**
+     * Checks if the distance limit has been reached
+     * @return true if distance limit is hit
+     */
+    private boolean checkDistance() {
+        return this.distanceTraveled == distance;
+    }
+
+    private void hitBlock() {
+        //hitBlock event
+        this.remove(RemovalReason.DISCARDED);
+    }
+
+    private void hitEntity() {
+        //hitEntity event
+        this.remove(RemovalReason.DISCARDED);
     }
 }
